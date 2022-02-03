0614d8b4729049fd2c8ffec4b9e9932e1994a6eb
Fernando <joaofernandoscsc@gmail.com>
2022-02-03 18:34:50 -0300

    Criando projeto

---
+++ src/main/java/com/jf/Padroesdeprojeto/model/ClienteRepository.java
@@ -0,0 +1,9 @@
+package com.jf.Padroesdeprojeto.model;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends CrudRepository<Cliente, Long> {
+
+}
